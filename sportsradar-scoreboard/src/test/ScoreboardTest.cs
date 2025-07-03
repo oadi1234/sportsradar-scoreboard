b740f80abf17a6f8f9f70dfe4feddc1126fb313a
jestemkartoflem <grzegorz.nowak1337@gmail.com>
2025-07-03 17:51:02 +0200

    Basic test layout

---
+++ sportsradar-scoreboard/src/test/ScoreboardTest.cs
@@ -0,0 +1,40 @@
+﻿namespace sportsradar_scoreboard.test;
+
+public class ScoreboardTest
+{
+    [Fact]
+    public void ShouldStartNewMatch()
+    {
+    }
+    
+    [Fact]
+    public void ShouldNotStartMatchWithTeamAlreadyPresentOnScoreboard()
+    {
+    }
+    
+    [Fact]
+    public void ShouldUpdateScore()
+    {
+    }
+
+    [Fact]
+    public void ShouldNotUpdateScoreWhenMatchIsFinished()
+    {
+        
+    }
+    
+    [Fact]
+    public void ShouldFinishMatch()
+    {
+    }
+    
+    [Fact]
+    public void ShouldDoNothingOnFinishingNonExistentMatch()
+    {
+    }
+    
+    [Fact]
+    public void ShouldPrintSummary()
+    {
+    }
+}
